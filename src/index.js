29c9dc7ad61d7da0e954b093c82faa779ff212ec
Thomas Noel <thomas.c.noel@gmail.com>
2024-06-10 06:19:36 -0700

    Set up basic structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 const html = require('./index.html');
+const Weather = require('./lib/Weather'); 
 
+(() => {
+  let weather = Weather();
+})();
---
+++ src/lib/Weather.js
@@ -0,0 +1,11 @@
+
+class Weather {
+  constructor() {
+    // Bad idea in the real world, don't ever do ever
+    this.key = "d1057962e72541a781c161811243005";
+  }
+}
+
+module.exports = {
+  Weather
+};
