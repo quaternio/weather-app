4f3e75d1d5d7e692cec410fff0c04d23bef873f0
Thomas Noel <tnoel@novadynamics.com>
2024-06-11 10:55:25 -0700

    Clean data and start Forecast class

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 const html = require('./index.html');
 const { Weather } = require('./lib/Weather'); 
 
+class Forecast {
+  constructor(weatherData) {
+    this.data = weatherData; 
+
+    this.town = this.data.town;
+    this.state = this.data.state;
+    this.time = this.data.time; // "YYY-mm-dd HH:MM"
+  }
+
+  populateDay(id) {
+    let day = document.getElementById(id);
+
+    // TODO Pick up here!
+  }
+}
+
 (async () => {
   let weather = new Weather();
-  console.log(await weather.getForecast());
+
+     
 })();
--- src/lib/Weather.js
+++ src/lib/Weather.js
@@ -6,14 +6,40 @@ class Weather {
     this.url = "http://api.weatherapi.com/v1/forecast.json"
   }
 
+  processData(data) {
+    let cleanData = {};
+
+    cleanData.town = data.location.name;
+    cleanData.state = data.location.region;
+    cleanData.time = data.location.localtime;
+
+    cleanData.forecast = [];
+    data.forecast.forecastday.forEach((item) => {
+      let day = {};
+
+      day.date = item.date;
+      day.weather = item.day.condition.text;
+      day.icon = item.day.condition.icon;
+      day.avgtemp_f = item.day.avgtemp_f;
+      day.chanceOfRain = item.day.daily_chance_of_rain;
+      day.precipitation_mm = item.day.totalprecip_mm;
+
+      cleanData.forecast.push(day);
+    });
+
+    return cleanData;
+  }
+
   async getForecast(place="97370", days=3) {
-    return await fetch(`${this.url}?key=${this.key}&q=${place}&days=${days}`).then((res) => {
+    let data = await fetch(`${this.url}?key=${this.key}&q=${place}&days=${days}`).then((res) => {
       if (!res.ok) {
         throw new Error(`HTTP Error. Status: ${res.status}`);
       } 
 
       return res.json();
     });
+
+    return this.processData(data);
   }
 }
 
